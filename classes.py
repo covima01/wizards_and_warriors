bc96975895190d0ee7961c469f8d5f01d8857070
covima01 <covima01@gmail.com>
2025-06-24 16:22:22 -0400

    Combat logic updates and sequencing

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+import random
 
 class Mage: # Gandalf = Mage("Gandalf", 100, 20, 20)
     def __init__(self, name, health, intelligence, wisdom):
@@ -6,21 +7,29 @@ class Mage: # Gandalf = Mage("Gandalf", 100, 20, 20)
         self.intelligence = intelligence
         self.wisdom = wisdom
     def cast_fireball(self, target):
-        damage = self.intelligence + (self.wisdom * 1.25)
+        damage = self.intelligence + (self.wisdom * (random.uniform(0.8, 1.2)))
         target.health -= damage
         print("======================================================================================================")
         print(f"{self.name} casts fireball dealing {damage} damage.")
         print("======================================================================================================")
-        print(f"{target.name}'s health is now {target.health}.")
-        print("======================================================================================================")
+        if target.health <= 0:
+            print(f"{target.name} is dead. Game over.")
+            return
+        else:
+            print(f"{target.name}'s health is now {target.health}.")
+            print("======================================================================================================")
     def cast_lightning(self, target):
-        damage = self.intelligence + (self.wisdom * 1.5)
+        damage = self.intelligence + (self.wisdom * (random.uniform(0.9, 1.4)))
         target.health -= damage
         print("======================================================================================================")
         print(f"{self.name} casts lightning dealing {damage} damage.")
         print("======================================================================================================")
-        print(f"{target.name}'s health is now {target.health}.")
-        print("======================================================================================================")
+        if target.health <= 0:
+            print(f"{target.name} is dead. Game over.")
+            return
+        else:
+            print(f"{target.name}'s health is now {target.health}.")
+            print("======================================================================================================")
 
 class Fighter: # Cloud = Fighter("Cloud", 125, 20, 20)
     def __init__(self, name, health, strength, rage):
@@ -34,7 +43,22 @@ class Fighter: # Cloud = Fighter("Cloud", 125, 20, 20)
         print("======================================================================================================")
         print(f"{self.name} swings his axe dealing {damage} damage.")
         print("======================================================================================================")
-        print(f"{target.name}'s health is now {target.health}.")
+        if target.health <= 0:
+            print(f"{target.name} is dead. Game over.")
+            return
+        else:
+            print(f"{target.name}'s health is now {target.health}.")
+            print("======================================================================================================")
+    def heavy_swing(self, target):
+        damage = self.strength + (self.rage * 1.5)
+        target.health -= damage
         print("======================================================================================================")
-
+        print(f"{self.name}'s heavy swing deals {damage} damage.")
+        print("======================================================================================================")
+        if target.health <= 0:
+            print(f"{target.name} is dead. Game over.")
+            return
+        else:
+            print(f"{target.name}'s health is now {target.health}.")
+            print("======================================================================================================")
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from classes import Mage, Fighter
 from inputs import character_select
 
@@ -6,16 +7,56 @@ def main():
     running = True
     Gandalf = Mage("Gandalf", 100, 20, 20)
     Cloud = Fighter("Cloud", 125, 20, 20)
-    character_select()
+
+    # Character selection
+    selection = input("Choose your character: 1 = Gandalf, 2 = Cloud\n")
+    while selection != "1" and selection != "2":
+        print("Unrecognized selection. Please choose 1 for Gandalf or 2 for Cloud\n")
+        selection = input("Choose your character: 1 = Gandalf, 2 = Cloud\n")
+    if selection == "1":
+        print("You've selected Gandalf.")
+        input("Hit enter to continue")
+    elif selection == "2":
+        print("You've selected Cloud")
+        input("Hit enter to continue")
+
     while running:
-        if Gandalf.health <= 0:
-            print("Gandalf is dead. Game over.")
-            return
-        elif Cloud.health <= 0:
-            print("Cloud is dead. Game over.")
-            return
-        else:
-            Gandalf.cast_fireball(Cloud)
+        # Playing as Gandalf
+        if selection == "1":
+            if Gandalf.health <= 0:
+                print("Gandalf is dead. Game over.")
+                return
+            elif Cloud.health <= 0:
+                print("Cloud is dead. Game over.")
+                return
+            else:
+                attack = random.randint(0,2)
+                if attack == 0:
+                    Gandalf.cast_fireball(Cloud)
+                    if Cloud.health <= 0:
+                        return
+                elif attack == 1:
+                    Gandalf.cast_lightning(Cloud)
+                    if Cloud.health <= 0:
+                        return
+                elif attack == 2:
+                    print("Gandalf missed")
+        # Playing as Cloud
+        elif selection == "2":
+            if Gandalf.health <= 0:
+                return
+            elif Cloud.health <= 0:
+                return
+            else:
+                attack = random.randint(0,2)
+                if attack == 0:
+                    Cloud.swing_axe(Gandalf)
+                elif attack == 1:
+                    Cloud.heavy_swing(Gandalf)
+                elif attack == 2:
+                    print("Cloud missed")
+                
+
 
 
 
