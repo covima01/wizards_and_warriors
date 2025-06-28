c09fff424c034273e7ce57dcf3afee30e5ee8cbe
covima01 <covima01@gmail.com>
2025-06-28 09:07:40 -0400

    Began working on enemies for campaign mode

---
+++ enemies.py
@@ -0,0 +1,4 @@
+
+Orcs = []
+Orc_Grunt = Orc("Orc Grunt", 75, 12)
+Orc_General = Orc("Orc General", 125, 18)
---
+++ enemy_classes.py
@@ -0,0 +1,32 @@
+import random
+from classes import border
+from characters import Gandalf
+
+class Orc:
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+    def overhead_smash(self, target):
+        damage = int(round(self.strength * (random.uniform(0.25, 0.5))))
+        target.health -= damage
+        if target.health <=0:
+            print(border)
+            print(f"{self.name}'s overhead smash deals {damage}. {target.name} has perished.".center(70))
+            print(border)
+        else:
+            print(border)
+            print(f"{self.name}'s overhead smash deals {damage} damage. {target.name}'s health is now {target.health}".center(70))
+            print(border)
+    def grip_of_death(self, target):
+        damage = int(round(self.strength * (random.uniform(0.5, 1.0))))
+        target.health -= damage
+        if target.health <= 0:
+            print(border)
+            print(f"{self.name}'s grip of death deals {damage}. {target.name} has perished.".center(70))
+            print(border)
+        else:
+            print(border)
+            print(f"{self.name}'s grip of death deals {damage} damage. {target.name}'s health is now {target.health}".center(70))
+            print(border)
+
