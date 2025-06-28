5559b413654d1af7b84763f8ae64894fdd4d91d3
covima01 <covima01@gmail.com>
2025-06-28 09:09:29 -0400

    Cleaned main function and implemented character selection function for ease of use

--- main.py
+++ main.py
@@ -2,22 +2,16 @@ import random
 from classes import *
 from combat import *
 from characters import *
+from inputs import character_selection
 
 
 def main():
-    selection = input("Choose your character: 1 = Gandalf, 2 = Cloud".center(70))
-    while selection != "1" and selection != "2":
-        print("Unrecognized selection. Please choose 1 for Gandalf or 2 for Cloud".center(70))
-        selection = input("Choose your character: 1 = Gandalf, 2 = Cloud".center(70))
-    if selection == "1":
-        input("You've selected Gandalf. Hit enter to do battle.".center(70))
-        print(border)
-        combat(Gandalf, Cloud)
-    elif selection == "2":
-        input("You've selected Cloud. Hit enter to do battle.".center(70))
-        print(border)
-        combat(Cloud, Gandalf)
-
+    character_selection()
+    character = character_selection("Choose your character-\n")
+    opponent = character_selection("Choose your opponent-\n")
+    print(f"{character.name} vs {opponent.name}")
+    from combat import combat
+    combat(character, opponent)
 
 if __name__ == "__main__":
     main()
