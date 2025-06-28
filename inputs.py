0d752d3bf9402c54451ad6c253a107d592a38b98
covima01 <covima01@gmail.com>
2025-06-28 09:08:48 -0400

    Adjusted character selection logic to accoutn for new characters

--- inputs.py
+++ inputs.py
@@ -1,17 +1,27 @@
+from classes import border, Wizard, Warrior
+from characters import Characters, Gandalf
 
-def character_select():
-    selection = input("Choose your character: 1 = Gandalf, 2 = Cloud\n")
-    while selection != "1" and selection != "2":
-        print("Unrecognized selection. Please choose 1 for Gandalf or 2 for Cloud\n")
-        selection = input("Choose your character: 1 = Gandalf, 2 = Cloud\n")
-    if selection == "1":
-        print("You've selected Gandalf.")
-        input("Hit enter to continue")
-        return "Gandalf"
-    elif selection == "2":
-        print("You've selected Cloud")
-        input("Hit enter to continue")
-        return "Cloud"
+
+def character_selection(prompt):
+    from combat import combat
+    print(prompt)
+    for character in Characters:
+        print(character.name)
+    selection = input("\nEnter character name: ")
+    while selection not in [character.name for character in Characters]:
+        print("That's not a character. Please choose from the list above.")
+        print("\n")
+        selection = input()
+    for character in Characters:
+        if character.name == selection:
+            print(border)
+            return character
+
+character = character_selection("Choose your character-\n")
+opponent = character_selection("Choose your opponent-\n")
+print(f"{character.name} vs {opponent.name}")
+from combat import combat
+combat(character, opponent)
 
 
 
