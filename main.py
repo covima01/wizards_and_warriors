3e26e60085e155be5e8749b014cb937b54d70cd4
covima01 <covima01@gmail.com>
2025-06-28 09:48:46 -0400

    Small syntax tweaks to include combat changes

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import random
 from classes import *
-from combat import *
+
 from characters import *
 from inputs import character_selection
 
 
 def main():
-    character_selection()
     character = character_selection("Choose your character-\n")
     opponent = character_selection("Choose your opponent-\n")
     print(f"{character.name} vs {opponent.name}")
     from combat import combat
     combat(character, opponent)
+
 
 if __name__ == "__main__":
     main()
