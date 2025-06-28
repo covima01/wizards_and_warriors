f944d942544024412a9b828978993b3017cac531
covima01 <covima01@gmail.com>
2025-06-28 09:04:58 -0400

    Added Yennefer, Jaina, Tidus, and Geralt

--- characters.py
+++ characters.py
@@ -1,5 +1,12 @@
+from classes import border
 from classes import Wizard, Warrior
 
-Gandalf = Wizard("Gandalf", 225, 150, 20, 18, 3)
-Cloud = Warrior("Cloud", 300, 100, 20, 18, 3)
-
+# Wizards
+Gandalf = Wizard("Gandalf", 225, 150, 20, 3)
+Yennefer = Wizard("Yennefer", 200, 200, 19, 3)
+Jaina = Wizard("Jaina", 250, 175, 22, 3)
+# Warriors
+Cloud = Warrior("Cloud", 350, 100, 22, 3)
+Geralt = Warrior("Geralt", 300, 100, 24, 3)
+Tidus = Warrior("Tidus", 400, 100, 20, 3)
+Characters = [Gandalf, Yennefer, Jaina, Cloud, Geralt, Tidus]
