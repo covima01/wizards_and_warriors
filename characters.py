602fe2557a8807e0045cba0ba89660675c2191a0
covima01 <covima01@gmail.com>
2025-06-28 09:46:51 -0400

    Created character list

--- characters.py
+++ characters.py
@@ -1,6 +1,7 @@
 from classes import border
 from classes import Wizard, Warrior
 
+
 # Wizards
 Gandalf = Wizard("Gandalf", 225, 150, 20, 3)
 Yennefer = Wizard("Yennefer", 200, 200, 19, 3)
@@ -9,4 +10,8 @@ Jaina = Wizard("Jaina", 250, 175, 22, 3)
 Cloud = Warrior("Cloud", 350, 100, 22, 3)
 Geralt = Warrior("Geralt", 300, 100, 24, 3)
 Tidus = Warrior("Tidus", 400, 100, 20, 3)
+
+
 Characters = [Gandalf, Yennefer, Jaina, Cloud, Geralt, Tidus]
+Wizards = [Gandalf, Yennefer, Jaina]
+Warriors = [Cloud, Geralt, Tidus]
