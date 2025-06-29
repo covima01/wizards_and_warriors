3ec5c97a68a393d262c0307bac6c6b0754075331
covima01 <covima01@gmail.com>
2025-06-29 14:03:44 -0400

    Ogres grr

--- enemies.py
+++ enemies.py
@@ -1,7 +1,8 @@
 from enemy_classes import Ogre
 
 
-Ogres = []
 Ogre_Grunt = Ogre("Orc Grunt", 75, 12)
 Ogre_General = Ogre("Orc General", 125, 18)
 Ogre_Warlord = Ogre("Ogre Warlord", 150, 20)
+
+Ogres = [Ogre_Grunt, Ogre_General, Ogre_Warlord]
