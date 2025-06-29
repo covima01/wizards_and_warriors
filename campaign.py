718fec84508bae0ab056921756b0f6696f019061
covima01 <covima01@gmail.com>
2025-06-29 14:02:44 -0400

    In process

--- campaign.py
+++ campaign.py
@@ -1,8 +1,15 @@
 import random
 import time
 from classes import border, Wizard, Warrior
-from enemies import Ogres
+from characters import Gandalf
+from enemy_classes import Ogre
 
 
-def campaign(player, Ogres):
-    
+def campaign(player, *enemies):
+    num_enemies = random.randint(1, 3)
+    enemies = [Ogre(f"Ogre{i+1}", 100, 20) for i in range(num_enemies)]
+    print(player)
+    for enemy in enemies:
+        print(enemy)
+
+campaign(Gandalf, Ogre)
