f3118980d8840cd87fc6976ce6c72ef90e3679be
covima01 <covima01@gmail.com>
2025-06-30 11:48:12 -0400

    Fixed enemy name type

--- enemies.py
+++ enemies.py
@@ -1,8 +1,8 @@
 from enemy_classes import Ogre
 
 
-Ogre_Grunt = Ogre("Orc Grunt", 75, 12)
-Ogre_General = Ogre("Orc General", 125, 18)
+Ogre_Grunt = Ogre("Ogre Grunt", 75, 12)
+Ogre_General = Ogre("Ogre General", 125, 18)
 Ogre_Warlord = Ogre("Ogre Warlord", 150, 20)
 
 Ogres = [Ogre_Grunt, Ogre_General, Ogre_Warlord]
