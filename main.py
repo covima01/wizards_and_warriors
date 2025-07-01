8ab6f5f61dc01cf0fa4ec39dcbf9dfbd2ec6477a
covima01 <covima01@gmail.com>
2025-07-01 07:05:01 -0400

    Added game mode selection input and campaign function

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import random
 from classes import *
 from constants import BORDER
 from characters import *
-from inputs import character_selection
+from inputs import character_selection, game_mode
 
 
 def main():
-    character = character_selection("Choose your character-\n")
-    opponent = character_selection("Choose your opponent-\n")
-    print(f"{character.name} vs {opponent.name}")
-    print(BORDER)
-    from combat import combat
-    combat(character, opponent)
+    from campaign import campaign
+    game_type = game_mode("Which mode do you want to play?")
+    if game_type == "Combat":
+        print("You've selected Hero Battle.")
+        character = character_selection("Choose your character-\n")
+        opponent = character_selection("Choose your opponent-\n")
+        print(f"{character.name} vs {opponent.name}")
+        print(BORDER)
+        from combat import combat
+        combat(character, opponent)
+    elif game_type == "Campaign":
+        print("You've selected Campaign")
+        campaign()
+
 
 
 if __name__ == "__main__":
