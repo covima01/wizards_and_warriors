b943dfcf0fddb134cdb191de03e31566f9fccc6c
covima01 <covima01@gmail.com>
2025-07-02 20:05:07 -0400

    Created orc, harpy, and goblin objects

--- enemies.py
+++ enemies.py
@@ -1,6 +1,21 @@
-from enemy_classes import Ogre
+from enemy_classes import *
 
+# Goblins (self, name, health, strength, intelligence, level, xp)
+Goblin_Trickster = Goblin("Goblin Trickster", 20, 10, 10, 1, 5 )
+Goblin_Bully = Goblin("Goblin Bully", 30, 12, 10, 1, 10)
+Goblin_Fat_Cat = Goblin("Goblin Fat-Cat", 100, 7, 5, 1, 15)
 
+# Harpies (self, name, health, strength, intelligence, level, xp)
+Harpy_Softbeak = Harpy("Softbeak Harpy", 30, 9, 11, 1, 5)
+Harpy_Terror = Harpy("Harpy Terror", 45, 11, 13, 1, 10)
+Harpy_Matriarch = Harpy("Harpy Matriarch", 80, 13, 15, 1, 15)
+
+# Orcs (self, name, health, strength, intelligence, level, xp)
+Orc_Warrior = Orc("Orc Warrior", 50, 12, 6, 1, 10)
+Orc_Captain = Orc("Orc Captain", 75, 15, 7, 1, 20)
+Orc_Warmonger = Orc("Orc Warmonger", 120, 17, 8, 1, 35)
+
+# Ogres (self, name, health, strength, intelligence, level, xp)
 Ogre_Grunt = Ogre("Ogre Grunt", 25, 12, 5)
 Ogre_General = Ogre("Ogre General", 50, 18, 10)
 Ogre_Warlord = Ogre("Ogre Warlord", 75, 20, 15)
